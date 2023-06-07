f2ee0bbe03f6eee890bc93452cb6b6ed38e4def1
Aleandro_Resendiz <ale_resendiz321@hotmail.com>
2023-06-07 11:49:52 -0600

    agregando constantes y objetos como nombre, modelo y precio

--- problemas-CAP-6/P1/P1.js
+++ problemas-CAP-6/P1/P1.js
@@ -1,3 +1,14 @@
-function crearLlave(nombre, modelo, precio ){
+const contenedor = document.querySelector(".flex-container");
 
+function crearLlave(nombre, modelo, precio ){
+    img = "llave.png"
+    nombre = `<h2> ${nombre} </h2>`;
+    modelo = `<h3> ${modelo} </h3>`;
+    precio = `<p>Precio: <b>${precio}</b></p>`;
+    return[nombre, modelo, precio, img];
 }
+
+const llave = crearLlave( "Llave1", "Modelo X", "33");
+
+contenedor.innerHTML = llave[0] + llave[1] + llave[2];
+
